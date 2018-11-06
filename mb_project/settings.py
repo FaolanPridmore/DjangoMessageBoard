1298a652225bca2321a760eff646cdec78b6fe51
FaolanPridmore <faolan.pridmore@outlook.com>
2018-11-06 08:46:03 -0700

    added tests

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3$-cr&xi#!=i%1mjflf$_mgs!vu-m-n9vzvi@6w&$h^+j)k@36'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Post
 
+
 class HomePageView(ListView):
     model = Post
     template_name = 'home.html'
